1b58643341f85c525c84ed3e93a735679407f81b
Will Grant <will.granty@yahoo.co.uk>
2021-09-24 14:48:52 +0100

    change to secret key

--- project_tracker/settings.py
+++ project_tracker/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import secrets
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'gd@lggl#p2)1hjs%blp6wg6g)=g(9rmy$a$fyfyka(9ii+9q4o=m*'
+SECRET_KEY = secrets.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
