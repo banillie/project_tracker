285a64aa58b8faa87f361ef06d8653ae159bb5ae
Will Grant <will.granty@yahoo.co.uk>
2021-09-28 12:12:28 +0100

    added links to navbar

--- ppdds/models.py
+++ ppdds/models.py
@@ -6,6 +6,7 @@ class PPDD(models.Model):
     first_name = models.CharField(max_length=40)
     last_name = models.CharField(max_length=40)
     # team = models.CharField(max_length=100, blank=True, null=True)
+    team = models.CharField(max_length=100, blank=True, null=True)
     role = models.CharField(max_length=100, blank=True, null=True)
     tele_no = models.CharField(max_length=1000, blank=True, null=True)
     live = models.BooleanField(default=True)
