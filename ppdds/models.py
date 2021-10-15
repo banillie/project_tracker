b5db47958fc509f25f4252e39963c764f170e0cd
Will Grant <will.granty@yahoo.co.uk>
2021-10-15 12:29:12 +0100

    addition of slug to ppdd model and slugify_instance_title in ppdd.utils

--- ppdds/models.py
+++ ppdds/models.py
@@ -1,19 +1,39 @@
 from django.db import models
 from django.urls import reverse
+from django.db.models.signals import pre_save, post_save
+from .utils import slugify_instance_title
 
 
 class PPDD(models.Model):
     first_name = models.CharField(max_length=40)
     last_name = models.CharField(max_length=40)
-    # team = models.CharField(max_length=100, blank=True, null=True)
+    slug = models.SlugField(blank=True, null=True, unique=True)
     team = models.CharField(max_length=100, blank=True, null=True)
     role = models.CharField(max_length=100, blank=True, null=True)
     tele_no = models.CharField(max_length=1000, blank=True, null=True)
     live = models.BooleanField(default=True)
 
     def get_absolute_url(self):
-        # would be good to understand the reverse in more detail.
         return reverse("ppdds:ppdd-detail", kwargs={"id": self.id})
+
+    def save(self, *arg, **kwargs):
+        super().save(*arg, **kwargs)
 
     def __str__(self):
         return self.first_name + ' ' + self.last_name
+
+
+def ppdd_pre_save(sender, instance, *args, **kwargs):
+    if instance.slug is None:
+        slugify_instance_title(instance, save=False)
+
+
+pre_save.connect(ppdd_pre_save, sender=PPDD)
+
+
+def ppdd_post_save(sender, instance, created, *args, **kwargs):
+    if created:
+        slugify_instance_title(instance, save=True)
+
+
+post_save.connect(ppdd_post_save, sender=PPDD)
--- ppdds/tests.py
+++ ppdds/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from django.utils.text import slugify
 
-# Create your tests here.
+from .models import PPDD
+from .utils import slugify_instance_title
+
+
+class PPDDTestCast(TestCase):
+    def setUp(self):
+        PPDD.objects.create(
+            first_name='Jimmy',
+            last_name='Jones'
+        )
+
+    def test_slug(self):
+        obj = PPDD.objects.all().order_by("id").first()
+        slug = obj.slug
+        self.assertEqual(slug, 'jimmy-jones')
