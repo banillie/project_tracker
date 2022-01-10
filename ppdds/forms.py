13a5f1a529269cd48ab9c99a0e23e4602ef621be
Will Grant <will.granty@yahoo.co.uk>
2022-01-10 10:57:58 +0000

    styling of ppdd create/update form

--- ppdds/forms.py
+++ ppdds/forms.py
@@ -4,10 +4,10 @@ from .models import PPDD
 
 
 class PPDDForm(forms.ModelForm):
-    first_name = forms.CharField(max_length=40)
-    last_name = forms.CharField(max_length=40)
-    role = forms.CharField(max_length=100, required=False)
-    tele_no = forms.CharField(max_length=1000, required=False)
+    # first_name = forms.CharField(max_length=40)
+    # last_name = forms.CharField(max_length=40)
+    # role = forms.CharField(max_length=100, required=False)
+    # tele_no = forms.CharField(max_length=1000, required=False)
 
     class Meta:
         model = PPDD
@@ -17,3 +17,9 @@ class PPDDForm(forms.ModelForm):
             "role",
             "tele_no",
         ]
+        widgets = {
+            "first_name": forms.TextInput(attrs={'class': 'form-control'}),
+            "last_name": forms.TextInput(attrs={'class': 'form-control'}),
+            "role": forms.TextInput(attrs={'class': 'form-control'}),
+            "tele_no": forms.TextInput(attrs={'class': 'form-control'}),
+        }
