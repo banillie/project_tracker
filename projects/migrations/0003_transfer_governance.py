8512d2f3d9bff31f2f9e372af59c23b06658c7db
Will Grant <will.granty@yahoo.co.uk>
2022-01-24 10:57:31 +0000

    link tier code

--- projects/migrations/0003_transfer_governance.py
+++ projects/migrations/0003_transfer_governance.py
@@ -3,6 +3,16 @@
 from django.db import migrations
 
 
+def link_tier(apps, schema_editor):
+    Project = apps.get_model('projects', 'Project')
+    Tier = apps.get_model('projects', 'Tier')
+    for project in Project.objects.all():
+        if project.governance is not None:
+            governance, created = Tier.objects.get_or_create(type=project.governance)
+            project.tier = governance
+            project.save()
+
+
 class Migration(migrations.Migration):
 
     dependencies = [
