cc310bf04467761f72fddb8d5cb8cb5546933a4f
Will Grant <will.granty@yahoo.co.uk>
2022-03-31 14:10:14 +0100

    stakeholders page tidy

--- stakeholders/admin.py
+++ stakeholders/admin.py
@@ -3,6 +3,16 @@ from simple_history.admin import SimpleHistoryAdmin
 
 from .models import Stakeholder, StakeholderOrg, DFTGroup
 
-admin.site.register(Stakeholder, SimpleHistoryAdmin)
+
+class StakeholderAdmin(SimpleHistoryAdmin):
+    # list_display = ['name', 'slug', 'timestamp', 'updated']
+    list_display = ['full_name', 'organisation', 'dft_group', 'slug']
+    search_fields = ['first_name', 'last_name', 'organisation__name', 'dft_group__name', 'team']
+
+    def full_name(self, obj):
+        return obj
+
+
+admin.site.register(Stakeholder, StakeholderAdmin)
 admin.site.register(StakeholderOrg, SimpleHistoryAdmin)
 admin.site.register(DFTGroup, SimpleHistoryAdmin)
