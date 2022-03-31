7cc3046b21e9642a3de47d481adc43e0be1e5c95
Will Grant <will.granty@yahoo.co.uk>
2022-03-31 15:22:03 +0100

    projects admin page tidy

--- projects/admin.py
+++ projects/admin.py
@@ -5,8 +5,9 @@ from simple_history.admin import SimpleHistoryAdmin
 
 class ProjectAdmin(SimpleHistoryAdmin):
     # list_display = ['name', 'slug', 'timestamp', 'updated']
-    list_display = ['name', 'tier', 'dft_group', 'updated']
-    search_fields = ['name', 'type', 'tier__type', ]
+    fields = ['name', 'slug', 'abbreviation', 'type', 'tier', 'dft_group', 'stage_name']
+    list_display = ['name', 'abbreviation', 'type', 'tier', 'dft_group']
+    search_fields = ['name', 'type', 'tier__type', 'abbreviation', 'dft_group__name']
 
 
 admin.site.register(Project, ProjectAdmin)
