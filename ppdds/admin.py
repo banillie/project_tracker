e63e54d138f28947f043956f5bc6194dc9234d55
Will Grant <will.granty@yahoo.co.uk>
2022-04-01 10:29:32 +0100

    ppdds page tidy

--- ppdds/admin.py
+++ ppdds/admin.py
@@ -3,4 +3,16 @@ from simple_history.admin import SimpleHistoryAdmin
 
 from .models import PPDD
 
-admin.site.register(PPDD, SimpleHistoryAdmin)
+
+class PPDDAdmin(SimpleHistoryAdmin):
+    # list_display = ['name', 'slug', 'timestamp', 'updated']
+    # fields = ['user', 'first_name', 'last_name', 'slug', 'team', 'role']
+    list_display = ['full_name', 'team', 'role']
+    ordering = ['last_name', ]
+    search_fields = ['first_name', 'last_name', 'team', 'role']
+
+    def full_name(self, obj):
+        return obj
+
+
+admin.site.register(PPDD, PPDDAdmin)
