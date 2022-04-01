324526007b4e21092da70c8a9547a36f3b2b3877
Will Grant <will.granty@yahoo.co.uk>
2022-04-01 10:11:11 +0100

    tidy of engagements page

--- engagements/admin.py
+++ engagements/admin.py
@@ -8,13 +8,29 @@ from simple_history.admin import SimpleHistoryAdmin
 
 class EngagementAdmin(SimpleHistoryAdmin):
     form = select2_modelform(Engagement)
-    list_display = ['date']
+    # fields = ['date', 'projects', 'stakeholders']
+    list_display = ['date', 'get_projects', 'get_stakeholders', 'get_topics']
+    ordering = ['date', ]
+    search_fields = [
+        'date',
+        'projects__name',
+        'stakeholders__first_name',
+        'stakeholders__last_name',
+        'topics__topic'
+    ]
 
+    def get_projects(self, obj):
+        return ",\n".join([p.name for p in obj.projects.all()])
 
-# class EngagementTopicAdmin(admin.ModelAdmin):
-#    """ Team Role admin view with modifications """
-#    model = EngagementTopic
-#    ordering = ['topic']
+    def get_stakeholders(self, obj):
+        return ",\n".join([str(s) for s in obj.stakeholders.all()])
+
+    def get_topics(self, obj):
+        return ",\n".join([t.topic for t in obj.topics.all()])
+
+# class EngagementAdmin(SimpleHistoryAdmin):
+#     form = select2_modelform(Engagement)
+#     list_display = ['date']
 
 
 admin.site.register(Engagement, EngagementAdmin)
