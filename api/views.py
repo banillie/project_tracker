ea4ecd8ddadbe9101b077eb58def71e965665f6e
Will Grant <will.granty@yahoo.co.uk>
2022-06-09 10:24:12 +0100

    use of model_to_dict

--- api/views.py
+++ api/views.py
@@ -1,10 +1,12 @@
 from django.http import JsonResponse
+from django.forms.models import model_to_dict
 
 from projects.models import Project
 
 
 def api_home(request, *args, **kwargs):
     model_data = Project.objects.all().order_by("?").first()
-    data = {'title': model_data.name}
+    # data = {}
+    data = model_to_dict(model_data, fields=['id'])
     return JsonResponse(data)
 
