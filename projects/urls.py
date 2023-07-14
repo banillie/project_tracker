3e3482305b31b0561b9f73651eb1a3044a117565
Will Grant <will.granty@yahoo.co.uk>
2023-07-14 15:30:12 +0100

    layout change to project detail and list

--- projects/urls.py
+++ projects/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import (
     project_detail_view,
     project_create_view,
-    project_update_view,
+    # project_update_view,
     project_delete_view,
     project_list_view,
 )
@@ -11,7 +11,7 @@ app_name = 'projects'
 urlpatterns = [
     path("", project_list_view, name='list'),
     path("create/", project_create_view, name='create'),
-    path("<slug:slug>/update/", project_update_view, name='update'),
+    path("<slug:slug>/update/", project_create_view, name='update'),
     path("<slug:slug>/delete/", project_delete_view, name='delete'),
     path("<slug:slug>/", project_detail_view, name='detail'),
 ]
--- projects/views.py
+++ projects/views.py
@@ -53,19 +53,22 @@ def project_delete_view(request, slug):
 def project_detail_view(request, slug):
     obj = get_object_or_404(Project, slug=slug)
     engage_qs = Engagement.objects.filter(projects__slug=slug).order_by('-date')
-    stake_qs = []
-    for x in engage_qs.all().values('stakeholders').distinct():
-        try:
-            sh = Stakeholder.objects.get(pk=x['stakeholders'])
-            if sh not in stake_qs:  # remove repeats
-                stake_qs.append(sh)
-        except ObjectDoesNotExist:
-            pass
-    stake_qs_ordered = sorted(stake_qs, key=operator.attrgetter('last_name'))
+    stakeholder_ids = engage_qs.values_list("stakeholders", flat=True).distinct()
+    stakeholder_qs = Stakeholder.objects.filter(pk__in=stakeholder_ids).order_by("last_name")
+    # stake_qs = []
+    # for x in engage_qs.all().values('stakeholders').distinct():
+    #     try:
+    #         sh = Stakeholder.objects.get(pk=x['stakeholders'])
+    #         if sh not in stake_qs:  # remove repeats
+    #             stake_qs.append(sh)
+    #     except ObjectDoesNotExist:
+    #         pass
+    # stake_qs_ordered = sorted(stake_qs, key=operator.attrgetter('last_name'))
     context = {
         "object": obj,
         "engagement_list": engage_qs,
-        "stakeholder_list": stake_qs_ordered,
+        "stakeholder_list": stakeholder_qs,
+        "hx_update_project_url": obj.get_update_url,
     }
     return render(request, "projects/detail.html", context)
 
@@ -102,38 +105,55 @@ def project_search_view(request):
     return render(request, "projects/search.html", context)
 
 
-@login_required
-def project_update_view(request, slug=None):
-    obj = get_object_or_404(Project, slug=slug)
-    form = ProjectForm(request.POST or None, instance=obj)
-    context = {
-        'form': form,
-        # 'object': obj,
-    }
-    if form.is_valid():
-        form.save()
-        return redirect('../')
-        # context['message'] = 'Date Saved'
-    # if request.htmx:
-    #     return render(request, "projects/partials/forms.html", context)
-    return render(request, "projects/create-update.html", context)
+# @login_required
+# def project_update_view(request, slug=None):
+#     obj = get_object_or_404(Project, slug=slug)
+#     form = ProjectForm(request.POST or None, instance=obj)
+#     context = {
+#         'form': form,
+#         # 'object': obj,
+#     }
+#     if form.is_valid():
+#         form.save()
+#         return redirect('../')
+#         # context['message'] = 'Date Saved'
+#     # if request.htmx:
+#     #     return render(request, "projects/partials/forms.html", context)
+#     return render(request, "projects/create-update.html", context)
+#
 
-
 @login_required
-def project_create_view(request):
+def project_create_view(request, slug=None):
     if not request.htmx:
         return Http404
 
-    project_form = ProjectForm(request.POST or None)
+    if not slug:
+        project_form = ProjectForm(request.POST or None)
+    else:
+        obj = get_object_or_404(Project, slug=slug)  # handles page not found.
+        project_form = ProjectForm(request.POST or None, instance=obj)
+
     context = {
-        "hx_create_project_url_list": reverse("projects:create"),
         "project_form": project_form,
     }
+    if not slug:
+        context["hx_create_project_url_list"] = reverse("projects:create")
+    else:
+        context["hx_update_project_url"] = obj.get_update_url
+
     if project_form.is_valid():
         project_form.save()
-        queryset = Project.objects.all().order_by('name')
-        context["object_list"] = queryset
-        return render(request, "projects/partials/hx_create_project_in_project_list.html", context)
+        if not slug:
+            queryset = Project.objects.all().order_by('name')
+            context["object_list"] = queryset
+            return render(request, "projects/partials/hx_create_project_in_project_list.html", context)
+        else:
+            context["object"] = obj
+            return render(
+                request,
+                "projects/partials/hx_update_project.html",
+                context,
+            )
     return render(request, "engagements/partials/hx_create_project_modal_form.html", context)
 
     # error_msg = None
